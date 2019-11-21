bde36ad01776e96ea2b47deb0712388f4bb54111
Alusio <aguignard37@gmail.com>
2019-11-21 10:18:25 -0800

    edit conf nginx

--- init-letsencrypt2.sh
+++ init-letsencrypt2.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(alusio.fr www.alusio.fr)
+domains=(disney-day.fr www.disney-day.fr)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
