a3f17a10a2be834445ca453d28e8a431769a7b5c
Alusio <aguignard37@gmail.com>
2020-06-11 00:24:54 -0700

    Add dev

--- blog/views/views.py
+++ blog/views/views.py
@@ -130,7 +130,7 @@ def SignUp(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
+            user.refresh_from_db()
             user.save()
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
