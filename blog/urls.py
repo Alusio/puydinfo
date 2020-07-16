56a7486eb5084ce445669a3680213aebcce6ea79
Alusio <aguignard37@gmail.com>
2020-07-16 01:49:44 -0700

    Update many things

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('calendrier', views.calendar, name='calendar'),
     path('period/<slug:slug>', views.period, name='period'),
-    path('', views.index_temp),
     path('show/<slug:slug>', views.shows, name="show"),
     path('animation/<slug:slug>', views.animation, name="animation"),
     path('ville/<slug:slug>', views.ville, name="ville"),
